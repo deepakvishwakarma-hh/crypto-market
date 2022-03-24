d86402628bf268cbc67414c68c5555a89067f121
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-03-24 12:20:52 +0530

    update Details : add New Detail area for Xs size screen

--- templates/Details.tsx
+++ templates/Details.tsx
@@ -1,5 +1,6 @@
 import { Coin } from "../utils/types";
 import { motion } from "framer-motion";
+import { useState } from 'react'
 import { useRouter } from "next/router";
 import ColorManager from "../utils/_mini";
 import { useAppDispatch } from "../redux-store/hooks";
@@ -15,6 +16,8 @@ const Detail = ({ data }: { data: Coin }) => {
     const { colorMode } = useColorMode();
     const color = useColorModeValue("white", '#2D3748')
     const text = useColorModeValue('#2D3748', "#E2E8F0")
+    const [isMoreDetailsTrue, setMoreDetails] = useState<boolean>(false)
+    const onWrapperClick = () => { setMoreDetails(!isMoreDetailsTrue) }
 
     const boxShadowBasesOnTheme = (colorMode == 'light')
         ? '1px 6px 15px -1px rgba(148,145,148,.3)'
@@ -32,15 +35,34 @@ const Detail = ({ data }: { data: Coin }) => {
         }
     }
 
+
+    const Mapper = ['supply', 'changePercent24Hr', 'priceUsd', 'volumeUsd24Hr', 'vwap24Hr'].map((value: any, index: number) => {
+        return (
+            <Flex flexWrap={'wrap'} px={3} py={1} key={index} justifyContent="space-between">
+                <Text color={"tomato"} p={1} fontWeight={500} textTransform={'capitalize'}>{value} : </Text>
+                <Text p={1} wordBreak={['break-all']}  >{data[value]}</Text>
+            </Flex >
+        )
+    })
+
     return (
-        <MotionWrap
-            userSelect={'none'} color={text} boxShadow={boxShadowBasesOnTheme} bgGradient={gradient} my={2} p={2} borderRadius={2}>
-            <Text p={2} flex={.5}>{rank}</Text>
-            <Text p={2} flex={2} >{name}</Text>
-            <Text display={['none', 'block']} color={'black'} p={2} flex={1}>${priceUsd}</Text>
-            <Text display={['none', 'block']} color="black" p={2} flex={1} >${changePercent24Hr}</Text>
-            <Button bg={color} onClick={onClick}>{">"}</Button>
-        </MotionWrap >
+        <>
+            <MotionWrap onClick={onWrapperClick}
+                userSelect={'none'} color={text} boxShadow={boxShadowBasesOnTheme} bgGradient={gradient} my={2} p={2} borderRadius={2}>
+                <Text p={2} flex={.5}>{rank}</Text>
+                <Text p={2} flex={2} >{name}</Text>
+                <Text display={['none', 'block']} color={'black'} p={2} flex={1}>${priceUsd}</Text>
+                <Text display={['none', 'block']} color="black" p={2} flex={1} >${changePercent24Hr}</Text>
+                <Button bg={color} onClick={onClick}>{">"}</Button>
+            </MotionWrap >
+
+            {isMoreDetailsTrue && <Flex borderRadius={2}
+                bg={(colorMode == 'light') ? 'white' : 'black'}
+                color={(colorMode !== 'light') ? 'white' : 'black'} flexDir={'column'} display={['flex', 'none']}>
+                {Mapper}
+            </Flex>}
+
+        </>
     )
 }
 
